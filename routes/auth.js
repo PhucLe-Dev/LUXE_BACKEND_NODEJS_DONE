8f91cc38d18428285ee303ce835b16c888bc49a4
Phúc Dev Frontend <phucle.415776@gmail.com>
2025-06-18 17:10:22 +0700

    edit file auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -10,7 +10,7 @@ router.post("/login", authControllers.loginUser);
 
 router.post("/refresh-token", authControllers.refreshToken);
 
-router.post("/logout", middlewaresController.verifyToken, authControllers.logoutUser);
+router.post("/logout", authControllers.logoutUser);
 
 router.post("/forgot-password", authControllers.forgotPassword);
 
